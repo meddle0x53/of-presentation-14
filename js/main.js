677bc3a121473abc062823d62a6b7c7ca85d3ab6
meddle <meddlesmerch@gmail.com>
2014-10-26 00:35:46 +0300

    Shortcuts added.

--- js/main.js
+++ js/main.js
@@ -9,14 +9,14 @@
     return ret;
   });
 
-  var appendSlides = function (data) {
+  function appendSlides (data) {
 
     var steps = data;
     var htmltemplate = $('#step-template').html();
     var htmltempl = Handlebars.compile(htmltemplate);
     steps.forEach(function (step, index) {
       var templ = htmltempl;
-      console.log(step);
+
       $.ajax({
         url: '/steps/' + step.uri,
         success: function (data) {
@@ -27,6 +27,56 @@
       });
     });
   };
+
+  document.addEventListener("keyup", function (event) {
+    console.log(event.keyCode)
+    if (event.keyCode === 83) {
+      // 's'
+      impressApi.goto('title');
+    }
+    if (event.keyCode === 84) {
+      // 't'
+      impressApi.goto('terms-1');
+    }
+    if (event.keyCode === 67) {
+      // 'c'
+      impressApi.goto('content');
+    }
+    if (event.keyCode === 65) {
+      // 'a'
+      impressApi.goto('companies-1');
+    }
+    if (event.keyCode === 77) {
+      // 'm'
+      impressApi.goto('menagement-1');
+    }
+    if (event.keyCode === 85) {
+      // 'u'
+      impressApi.goto('about-us-1');
+    }
+    if (event.keyCode === 87) {
+      // 'w'
+      impressApi.goto('the-world-1');
+    }
+    if (event.keyCode === 70) {
+      // 'f'
+      impressApi.goto('freedom-1');
+    }
+    if (event.keyCode === 79) {
+      // 'o'
+      impressApi.goto('coda-1');
+    }
+    if (event.keyCode === 69) {
+      // 'e'
+      impressApi.goto('coda-2');
+    }
+
+
+    if (event.keyCode === 66) {
+      // 'b'
+      window.history.back()
+    }
+  }, false);
 
   window.Presentation = {
     appendSlides: appendSlides
